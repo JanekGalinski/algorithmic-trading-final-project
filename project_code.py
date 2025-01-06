0b9668680e0bb2c85bfb2eaecdd90855e1ee478d
Jan Galiński <jangalinskiphoto@gmail.com>
2025-01-06 19:05:04 +0100

    Libraries import

--- project_code.py
+++ project_code.py
@@ -6,3 +6,20 @@
 #Individual Work Project
 #"Multiple signals strategy"
 #-----------------------------------------------------------------------------------------------------------------------------
+
+# %%
+#-----------------------------------------------------------------------------------------------------------------------------
+# 1) Importing libraries
+#-----------------------------------------------------------------------------------------------------------------------------
+
+import backtrader as bt
+import yfinance as yf
+import pandas as pd
+import datetime
+
+from sklearn.decomposition import PCA
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+# %%
